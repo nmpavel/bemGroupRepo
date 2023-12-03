0908d1d1f342c46458f0cdc0c0272e75fd52f609
nmpavel <dustopavel@gmail.com>
2023-12-03 17:37:38 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Students from "@/views/Students";
+
+export default function Home() {
+  return (
+    <div className=" flex justify-center items-center w-full h-screen ">
+      <Students/>
+    </div>
+  )
+}
